8d160c9dd21f0d638755a5f63310a507b7c7a049
ROSitashvili <richard.osita@outlook.com>
2017-12-29 23:38:17 -0500

    BIG THINGS ARE HAPPENING

--- Contact.java
+++ Contact.java
@@ -1,20 +1,17 @@
 import java.time.LocalDateTime;
+import org.json.simple.JSONObject;
 
 public class Contact{
-  private String firstName;
-  private String lastName;
-  private String email;
-  private String phoneNumber;
-  private final LocalDateTime initDate;//Date met
+  private JSONObject person = new JSONObject();
   private LocalDateTime nextMeet;
 
 //CONSTRUCTORS
   public Contact(String fN, String lN, String el, String pN){
-    firstName = fN;
-    lastName = lN;
-    email = el;
-    phoneNumber = pN;
-    initDate = LocalDateTime.now();
+    person.put("firstName",fN);
+    person.put("lastName",lN);
+    person.put("email",el);
+    person.put("phoneNumber",pN);
+    person.put("initDate",LocalDateTime.now());
   }
 
   public Contact(String fN, String lN, String el){
@@ -25,21 +22,21 @@ public class Contact{
   }
 
 
-  //GET/SET METHODS
+  //GET METHODS
 
   public String getFirstName(){
-    return firstName;
+    return (String)person.get("firstName");
   }
   public String getLastName(){
-    return lastName;
+    return (String)person.get("lastName");
   }
   public String getEmail(){
-    return email;
+return (String)person.get("email");
   }
   public String getPhoneNumber(){
-    return phoneNumber;
+return (String)person.get("phoneNumber");
   }
   public String getDateMet(){
-    return initDate.toString();
+return (String)person.get("initDate");
   }
 }
--- theNetworker.java
+++ theNetworker.java
@@ -1,7 +1,13 @@
+import java.io.File;
+import java.io.IOException;
 public class theNetworker{
   public static void main(String args[]){
-      System.out.println("HELLO");
-      Contact bill = new Contact("bill","myers","email@outlook.ca");
-      System.out.println(bill.getDateMet());
+    try {//Creates file for storage of contacts
+      File contactInformation = new File("theNetworkerContactInformation.txt");
+      contactInformation.createNewFile();
+    }
+    catch(IOException ioe) {
+       ioe.printStackTrace();
+    }
   }
 }
